2d6398cc1a17a26f386c7f10fddf597f58e31c46
ishan288 <ishan288@gmail.com>
2019-03-09 12:24:41 +1100

    modified groovy

--- Script1.groovy
+++ Script1.groovy
@@ -1,4 +1,4 @@
 node {
   stage 'Run JMeter Test'
-  sh 'C:/Users/isingla/Downloads/apache-jmeter-5.1/bin/jmeter.sh -n -t C:/Users/isingla/Desktop/myproject/Jmeter1.jmx -l test.jtl'
+  sh 'C:/Users/isingla/Downloads/apache-jmeter-5.1/bin/jmeter.sh -n -t C:/Users/isingla/Desktop/myproject/Jmeter1.jmx -Jusers=10 -l test.jtl'
 }
